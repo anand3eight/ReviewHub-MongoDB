01d05ef46ca3f673f4c3e1b70af807312aedff68
anand3eight <128626864+anand3eight@users.noreply.github.com>
2023-04-09 19:47:45 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,9 +1,4 @@
 import streamlit as st
-import sys
-sys.path.insert(0, '/Users/anand/PycharmProjects/DbmsProject/LoginPage/DBOperations')
-import DBUserOperations as DB
-
 if __name__ == "__main__" :
     x = st.empty()
-    dbObj = DB.DBUser()
     st.markdown("<h1 style='text-align: center; color: white;'>Welcome to Google Reviews⭐️</h1>", unsafe_allow_html=True)
